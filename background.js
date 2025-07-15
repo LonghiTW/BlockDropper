b9342967c583c6911305c136e96608c76a94a1b0
LonghiTW <165005419+LonghiTW@users.noreply.github.com>
2025-07-15 11:09:09 +0800

    Fix: solve the problem that ESC do not work properly on some web pages

--- background.js
+++ background.js
@@ -5,4 +5,11 @@ chrome.commands.onCommand.addListener((command) => {
       chrome.tabs.sendMessage(tabs[0].id, { action: "pickColor" });
     });
   }
+  
+  if (command === "clear-result") {
+    // 向當前活動標籤頁發送訊息，通知 content.js 執行操作
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "clearResult" });
+    });
+  }
 });
